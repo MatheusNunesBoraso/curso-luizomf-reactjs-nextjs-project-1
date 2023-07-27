778192912aae1d8070d0eecc22414b464276b611
Matheus Nunes Boraso <matheusnunesbrasil@outlook.com>
2023-07-27 14:06:48 -0300

    feat: :sparkles: #S2-17 - lifecycle methods - part 1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Component } from "react";
 
 class App extends Component {
   state = {
+    counter: 0,
     posts: [
       {
         id: 1,
@@ -22,9 +23,33 @@ class App extends Component {
     ],
   };
 
+  timeoutUpdate = null;
+
+  componentDidMount() {
+    this.handleTimeout();
+  }
+
+  componentDidUpdate() {
+    clearTimeout(this.timeoutUpdate);
+    this.handleTimeout();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeoutUpdate);
+  }
+
+  handleTimeout = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = "teste - 1";
+    this.timeoutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 });
+    }, 1000);
+  };
+
   render() {
     return (
       <div className="App">
+        <h1>{this.state.counter}</h1>
         {this.state.posts.map((e, index) => {
           return (
             <div key={index}>
